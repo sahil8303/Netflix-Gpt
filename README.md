971a62ebd1a7b2e94c095151baf912ada108c616
sahil8303 <sahilkawadse2003@gmail.com>
2026-06-17 16:11:48 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Netflix Gpt
+
+
+
+#Features
+-Login/Signup
+    -Sign in/Sign up form
+    -redirect to Browse Page
+ -Browse(after authentication)
+      -Header
+      -Main Movie
+            -Trailer in background
+            --Title & Description
+            -Movie Suggestion
+                   -MovieLists *N
+
+-NetflixGpt
+   -Search bar
+   -Movie Suggestions
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-black text-white">
+      Hello
+      </div>
+    </>
+  )
+}
+
+export default App
